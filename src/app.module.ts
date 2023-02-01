ee030b88fdb780f5a16e063db5c0ac2e96611ce5
Daniel J. Antonio <djedidiaha@gmail.com>
2023-02-01 10:57:36 +0700

    employee crud

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:asdfasdf@cluster0.yfoyf.mongodb.net/?retryWrites=true&w=majority',
+      'mongodb+srv://admin:asdfasdf@cluster0.yfoyf.mongodb.net/employee?retryWrites=true&w=majority',
     ),
+    EmployeesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
